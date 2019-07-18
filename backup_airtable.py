a0473cd256d4038e7bb51b94636fc20b7c140a0c
ryanku98 <ryanku98@gmail.com>
2019-07-18 17:09:59 -0400

    Change to openpyxl

--- backup_airtable.py
+++ backup_airtable.py
@@ -1,59 +1,99 @@
 import requests # .get()
-import xlwt     # create excel spreadsheets
-import sys      # .exit()
-import datetime # datetime
-import os       # .remove()
-
-# Configure AirTable authentication
-at_url = open("airtable_url.txt", "r")
-airtable_api_key = open("airtable_config.txt", "r")
-AIRTABLE_URL = at_url.read()
-headers = {"Authorization": "Bearer " + airtable_api_key.read()}
-at_url.close()
-airtable_api_key.close()
-
-# Retrieve AirTable data
-res = requests.get(AIRTABLE_URL, headers=headers)
-if res.status_code != 200:
-    print("Error: status code", res.status_code, "\nExiting...")
-    sys.exit()
-
-# Create xlwt workbook
-wb = xlwt.Workbook()
-ws = wb.add_sheet("Resume Backup");
-
-# Write data into corresponding columns
-excel_header_dictionary = {}
-count = 1
-headers = 0
-for record in res.json()['records']:
-    for k, v in record['fields'].items():
-        if k not in excel_header_dictionary:
-            # Add headers to dictionary and write to excel file
-            excel_header_dictionary.update({k : headers})
-            ws.write(0, headers, k)
-            headers += 1
-        ws.write(count, excel_header_dictionary[k], v)
-    count += 1
-
-# Create name of backup file with timestamp and save
-time_stamp = datetime.datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
-file_name = "Backup_test_" + time_stamp + ".xls"
-wb.save(file_name)
-
-# Login to Box
+from openpyxl import Workbook, load_workbook
+from sys import exit
+from datetime import datetime
+import os # remove, rename, path.exists
+from json import loads
 from boxsdk import JWTAuth
 from boxsdk import Client
-# Import JWT auth object with config file
-sdk = JWTAuth.from_settings_file('box_config.json')
-client = Client(sdk) # authenticated client
+from glob import glob
 
-file_path = file_name
-# Get folder ID
-f_id = open("box_folder_id.txt", "r")
-folder_id = f_id.read()
-f_id.close()
-box_file = client.folder(folder_id).upload(file_path, file_name) # upload backup to backup folder on Box
+last_upload_name = "last_uploaded.xlsx"
 
-# After upload, remove from local directory
-os.remove(file_name)
+def backup():
+    # Configure AirTable authentication
+    at_url = open("config/airtable_url.txt", "r")
+    at_api_key = open("config/airtable_config.txt", "r")
+
+    # Retrieve AirTable data
+    res = requests.get(at_url.read(), headers={"Authorization": "Bearer " + at_api_key.read()})
+    at_url.close()
+    at_api_key.close()
+    if res.status_code != 200:
+        print("ERROR: could not connect to AirTable - status code", res.status_code, "\nExiting...")
+        exit()
+
+    # Create openpyxl workbook
+    wb = Workbook()
+    ws = wb.active
+
+    # Write data into corresponding columns
+    excel_header_dictionary = {}
+    record_row = 2
+    num_headers = 1
+    for record in res.json()['records']:
+        for k, v in record['fields'].items():
+            if k not in excel_header_dictionary:
+                # Add headers to dictionary and write to excel file if didn't previously exist
+                excel_header_dictionary.update({k : num_headers})
+                ws.cell(row=1, column=num_headers, value=k)
+                num_headers += 1
+            # Write values under appropriate column
+            if isinstance(v, list): # if v is a list
+                ws.cell(row=record_row, column=excel_header_dictionary[k], value=", ".join(v))
+            else:
+                ws.cell(row=record_row, column=excel_header_dictionary[k], value=v)
+        record_row += 1
+
+    # Create name of backup file with timestamp and save
+    time_stamp = datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+    file_name_template = "AirTable_Backup_"
+    file_name = file_name_template + time_stamp + ".xlsx"
+    wb.save(filename=file_name)
+
+    # Login to Box
+    # Import JWT auth object with config file
+    sdk = JWTAuth.from_settings_file('config/box_config.json')
+    client = Client(sdk) # authenticated client
+    # If error occurs here, backup file persists and will be uploaded during next successful run
+
+    # Upload any previous backups if they failed
+    backups = glob(file_name_template + "*")    # Retrieves non-uploaded backups in order as list
+    backups.append(file_name)                   # Add current backup to list
+    for backup_file in backups:
+        if not same_as_last(backup_file, rows=record_row, cols=num_headers):
+            upload(client=client, file_name=backup_file)
+        else: # Delete without upload if the same
+            os.remove(backup_file)
+
+def upload(client, file_name):
+    # Get folder ID
+    f_id = open("config/box_folder_id.txt", "r")
+    folder_id = f_id.read()
+    f_id.close()
+
+    # Upload backup to backup folder on Box
+    box_file = client.folder(folder_id).upload(file_name)
+    # Set uploaded file as last_uploaded
+    if os.path.exists(last_upload_name):
+        os.remove(last_upload_name)
+    os.rename(file_name, last_upload_name)
+
+def same_as_last(doc, rows, cols):
+    if not os.path.exists(last_upload_name):
+        return False
+    wb1 = load_workbook(filename=last_upload_name)
+    wb2 = load_workbook(filename=doc)
+    ws1 = wb1.active
+    ws2 = wb2.active
+    for row in range(1, rows+1):
+        for col in range(1, cols+1):
+            if ws1.cell(row=row, column=col).value != ws2.cell(row=row, column=col).value:
+                return False # Return false if found mismatched cell
+    return True
+
+if __name__ == "__main__":
+    backup()
+    # doc1 = "AirTable_Backup_2019-07-18--11-53-56.xlsx"
+    # doc2 = "AirTable_Backup_2019-07-18--11-54-13.xlsx"
+    # print(same_as_last(doc=doc2, rows=71, cols=14))
