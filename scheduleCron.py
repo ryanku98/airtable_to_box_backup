7397a8561b3f8fb719e74973725986f86330d24b
ryanku98 <ryanku98@gmail.com>
2019-07-19 09:38:38 -0400

    job scheduling for Heroku

---
+++ scheduleCron.py
@@ -0,0 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from backup_airtable import backup
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', seconds=30)
+# @sched.scheduled_job('cron', hour=12, timezone='America/New_York')
+def backup_table():
+    backup()
+
+sched.start()
