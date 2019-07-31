f6cb4565b31011e51a81eeefb044d5983abe0508
Ryan Ku <ryanku98@gmail.com>
2019-07-31 16:19:59 -0400

    Run daily

--- scheduleCron.py
+++ scheduleCron.py
@@ -3,8 +3,8 @@ from backup_airtable import backup
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', seconds=30)
-# @sched.scheduled_job('cron', hour=12, timezone='America/New_York')
+# @sched.scheduled_job('interval', seconds=30)
+@sched.scheduled_job('cron', hour=12, timezone='America/New_York')
 def backup_table():
     backup()
 
