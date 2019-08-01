9bc4592de71853f37741c75e7a5d3fa3cc1e5a78
Ryan Ku <ryanku98@gmail.com>
2019-08-01 17:07:51 -0400

    Changes to work with Heroku's ephemeral filesystem

--- backup_airtable.py
+++ backup_airtable.py
@@ -8,12 +8,17 @@ from boxsdk import JWTAuth, Client
 from glob import glob
 
 last_upload_name = "last_uploaded.xlsx"
+airtable_url_path = "config/airtable_url.txt"
+airtable_config_path = "config/airtable_config.txt"
+box_config_path = "config/box_config.json"
+box_folder_id_path = "config/box_folder_id.txt"
+box_last_folder_id_path = "config/box_last_folder_id.txt"
 client = None
 
 def backup():
     # Configure AirTable authentication
-    at_url = open("config/airtable_url.txt", "r")
-    at_api_key = open("config/airtable_config.txt", "r")
+    at_url = open(airtable_url_path, "r")
+    at_api_key = open(airtable_config_path, "r")
 
     # Retrieve AirTable data
     res = requests.get(at_url.read(), headers={"Authorization": "Bearer " + at_api_key.read()})
@@ -53,35 +58,85 @@ def backup():
 
     # Login to Box
     # Import JWT auth object with config file
-    sdk = JWTAuth.from_settings_file('config/box_config.json')
-    global client
-    client = Client(sdk) # authenticated client
-    # If error occurs here, backup file persists and will be uploaded during next successful run
+    sdk = JWTAuth.from_settings_file(box_config_path)
+    try:
+        global client
+        client = Client(sdk) # authenticated client
+        # If error occurs here, backup file persists and will be uploaded during next successful run
+    except:
+        return
 
     # Upload any previous backups if they failed
     backups = glob(file_name_template + "*")    # Retrieves non-uploaded backups in order as list
     backups.append(file_name)                   # Add current backup to list
+    download_last()
     for backup_file in backups:
         if os.path.exists(backup_file):
             if not same_as_last(backup_file, rows=record_row, cols=num_headers):
                 upload(client=client, file_name=backup_file)
-                print("EVENT: File", backup_file, "uploaded")
             else: # Delete without upload if the same
                 os.remove(backup_file)
                 print("EVENT: File", backup_file, "not uploaded, identical to last")
 
+    upload_last()
+
+# download last_uploaded.xlsx from special folder in Box - snapshot of most recently backed-up version
+def download_last():
+    remove_last_local() # remove local last_uploaded.xlsx if it exists
+
+    f_id = open(box_last_folder_id_path, "r")
+    folder_id = f_id.read()
+    f_id.close()
+
+    # get file id
+    items_iter = client.folder(folder_id).get_items()
+    try:
+        item = items_iter.next()
+        file_id = item.id
+
+        # download using file id
+        box_file = client.file(file_id=file_id).get()
+        output_file = open(box_file.name, "wb")
+        box_file.download_to(output_file)
+        print("EVENT: File", box_file.name, "downloaded from Box with ID:", file_id)
+    except:
+        # no items
+        print("EVENT:", last_upload_name, "not found")
+
 def upload(client, file_name):
     # Get folder ID
-    f_id = open("config/box_folder_id.txt", "r")
+    f_id = open(box_folder_id_path, "r")
     folder_id = f_id.read()
     f_id.close()
 
     # Upload backup to backup folder on Box
-    box_file = client.folder(folder_id).upload(file_name)
-    # Set uploaded file as last_uploaded
-    if os.path.exists(last_upload_name):
-        os.remove(last_upload_name)
-    os.rename(file_name, last_upload_name)
+    try:
+        box_file = client.folder(folder_id).upload(file_name)
+        print("EVENT: File", file_name, "uploaded with ID:", box_file.id)
+        # Set uploaded file as last_uploaded
+        remove_last_local()
+        os.rename(file_name, last_upload_name)
+    except:
+        print("EVENT: Error uploading file", file_name, "- possible name collision. Deleting...")
+        os.remove(file_name)
+
+def upload_last():
+    f_id = open(box_last_folder_id_path, "r")
+    folder_id = f_id.read()
+    f_id.close()
+
+    # empty last_uploaded folder to maintain only 1 object when backup() ends
+    items_iter = client.folder(folder_id=folder_id).get_items()
+    try:
+        for item in items_iter:
+            client.file(file_id=item.id).delete()
+    except:
+        # folder already empty
+        pass
+
+    box_file = client.folder(folder_id).upload(last_upload_name)
+    print("EVENT: File", last_upload_name, "uploaded with ID:", box_file.id)
+    remove_last_local()
 
 def same_as_last(doc, rows, cols):
     if not os.path.exists(last_upload_name):
@@ -95,6 +150,10 @@ def same_as_last(doc, rows, cols):
             if ws1.cell(row=row, column=col).value != ws2.cell(row=row, column=col).value:
                 return False # Return false if found mismatched cell
     return True
+
+def remove_last_local():
+    if os.path.exists(last_upload_name):
+        os.remove(last_upload_name)
 
 if __name__ == "__main__":
     backup()
